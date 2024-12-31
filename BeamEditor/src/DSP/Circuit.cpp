b66f3c2dcf04a6ddfccb0e100496b33858f328b5
Beam Audio <beamempresa@gmail.com>
2024-12-31 14:55:45 +0100

    Update Circuit.cpp

--- BeamEditor/src/DSP/Circuit.cpp
+++ BeamEditor/src/DSP/Circuit.cpp
@@ -3,18 +3,16 @@
 #include "Circuit.h"
 
 Circuit::Circuit() {
-    this->inChunk.fill(0.0);
-    this->outChunk.fill(0.0);
+    
 }
 
 Circuit::Circuit(const vector<Component> components) {
     this->components = components;
-    this->inChunk.fill(0.0);
-    this->outChunk.fill(0.0);
+    
 }
 
-array<double, 100> Circuit::processChunk(const array<double, 100>& inChunk) {
-    array<double, 100> intermediateChunk = inChunk;
+vector<double> Circuit::processChunk(const vector<double>& inChunk) {
+    vector<double> intermediateChunk = inChunk;
     for (Component c : components) {
         intermediateChunk = c.processChunk(intermediateChunk);
     }
