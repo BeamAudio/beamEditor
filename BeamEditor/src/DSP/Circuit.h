73190f157f6a01bc8c35dec4966dc03a50051be5
Beam Audio <beamempresa@gmail.com>
2024-12-31 13:50:08 +0100

    Update Circuit.h
    divided into header file

--- BeamEditor/src/DSP/Circuit.h
+++ BeamEditor/src/DSP/Circuit.h
@@ -1,43 +1,26 @@
-#include<array>
-#include<vector>
-#include<Component.h>
+// Circuit.h
+
+#ifndef CIRCUIT_H
+#define CIRCUIT_H
+
+#include <array>
+#include <vector>
+#include "Component.h"
 
 using namespace std;
 
-//The circuit class is responsible for taking the audio input from the mixer and processing the output 
+// The circuit class is responsible for taking the audio input from the mixer and processing the output
 
-//The mixer controls the circuits inside it, so it will be the only element writing to the components
-
+// The mixer controls the circuits inside it, so it will be the only element writing to the components
 
 class Circuit {
-    private: 
+private:
+    vector<Component> components;
 
-
-        vector<Component> components;
-
-    public:
-        Circuit() {
-            this->inChunk.fill(0.0);
-            this->outChunk.fill(0.0);
-        }
-
-        Circuit(const vector<Component> components) {
-            this->components = components;
-            this->inChunk.fill(0.0);
-            this->outChunk.fill(0.0);
-        }
-
-        array<double,100> processChunk(const array<double,100>& inChunk) {
-
-            array<double, 100> intermediateChunk;
-            intermediateChunk = inChunk;
-                for(Component c : components) {
-                    intermediateChunk = c.processChunk(intermediateChunk);
-                }
-            return intermediateChunk;
-
-        }
-
-        
-        
+public:
+    Circuit();
+    Circuit(const vector<Component> components);
+    array<double, 100> processChunk(const array<double, 100>& inChunk);
 };
+
+#endif // CIRCUIT_H
